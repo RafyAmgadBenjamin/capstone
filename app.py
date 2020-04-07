bec7aba1150f499bbe8a42816445c7f8a455df37
Rafy <rafy.amgadbenjamin@gmail.com>
2020-04-07 21:15:43 +0200

    enable cors

--- app.py
+++ app.py
@@ -9,6 +9,18 @@ def create_app(test_config=None):
     app = Flask(__name__)
     CORS(app)
 
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
+
     return app
 
 
