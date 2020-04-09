7ff1d84c32d73595f7f7aa4d44e7021645308ae9
Rafy <rafy.amgadbenjamin@gmail.com>
2020-04-09 20:27:52 +0200

    Creating the models

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 
 database_name = "capstoneDB"
@@ -22,3 +23,30 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    migrate = Migrate(app, db)  # migrations instantiation
+
+
+ActorMovie = db.Table(
+    "ActorMovie",
+    db.Column("id", Integer, primary_key=True),
+    db.Column("movieId", Integer, db.ForeignKey("Movie.id")),
+    db.Column("actorId", Integer, db.ForeignKey("Actor.id")),
+)
+
+
+class Movie(db.Model):
+    __tablename__ = "Movie"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    release_date = db.Column(db.DateTime(), nullable=True)
+    actors = db.relationship("Actor", secondary=ActorMovie, backref="movie", lazy=True)
+
+
+class Actor(db.Model):
+    __tablename__ = "Actor"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer, nullable=True)
+    gender = db.Column(db.Char, nullable=True)
+    movies = db.relationship("Movie", secondary=ActorMovie, backref="actor", lazy=True)
+
